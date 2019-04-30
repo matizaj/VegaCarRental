179f2c776439d2b710c9d1122178a9a7ce1e6d21
mzajac <matzaj101@gmail.com>
2019-04-30 22:47:23 +0200

    refactoring

---
+++ Vega/Resources/SaveVehicleResources.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vega.Resources
+{
+    public class SaveVehicleResources
+    {
+        public int Id { get; set; }
+        public int ModelId { get; set; }
+        public bool IsRegistered { get; set; }
+        public ContactResource Contact { get; set; }
+        public ICollection<int> Features { get; set; }
+        public SaveVehicleResources()
+        {
+            Features = new Collection<int>();
+        }
+    }
+}
