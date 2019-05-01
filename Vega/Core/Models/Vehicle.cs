45a5aa96b43884b21b3923834c3b46137941bd13
mzajac <matzaj101@gmail.com>
2019-05-01 23:47:51 +0200

    change infrastructure for dependency inversion pronciple cd

---
+++ Vega/Core/Models/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vega.Core.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public int ModelId { get; set; }
+        public Model Model { get; set; }
+        public bool IsRegistered { get; set; }
+        public string ContactName { get; set; }
+        public string ContactPhone { get; set; }
+        public string ContactEmail { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public ICollection<VehicleFeature> Features { get; set; }
+
+        public Vehicle()
+        {
+            Features = new Collection<VehicleFeature>();
+        }
+    }
+}
---
+++ Vega/Core/Models/VehicleFeature.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vega.Core.Models
+{
+    public class VehicleFeature
+    {
+        public int VehicleId { get; set; }
+        public int FeatureId { get; set; }
+        public Vehicle Vehicle { get; set; }
+        public Feature Feature { get; set; }
+    }
+}
