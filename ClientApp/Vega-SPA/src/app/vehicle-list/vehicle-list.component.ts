5df7da9a00831191883b3a2c3e479f143bbff403
Mateusz Zajac <TRBC37@motorolasolutions.com>
2019-05-06 18:16:55 +0200

    vehicle list changes

--- ClientApp/Vega-SPA/src/app/vehicle-list/vehicle-list.component.ts
+++ ClientApp/Vega-SPA/src/app/vehicle-list/vehicle-list.component.ts
@@ -9,10 +9,13 @@ import { Vehicle } from '../models/vehicle';
 })
 export class VehicleListComponent implements OnInit {
   vehicles: Vehicle[];
+  makes: any[];
   constructor(private makeServices: MakeService) { }
 
   ngOnInit() {
     this.makeServices.getAllVehicles().subscribe((x: Vehicle[]) => this.vehicles = x);
+    this.makeServices.getMakes().subscribe((data: any[]) => this.makes = data);
+
   }
 
 }
